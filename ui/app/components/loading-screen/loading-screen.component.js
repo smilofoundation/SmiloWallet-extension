26692088fb691a231ade02be3d8f9d80e7818ff1
Unknown <mail@mathyn.com>
2019-02-05 10:30:10 +0100

    Rebranded loading indicator

--- ui/app/components/loading-screen/loading-screen.component.js
+++ ui/app/components/loading-screen/loading-screen.component.js
@@ -14,7 +14,7 @@ class LoadingScreen extends Component {
       h('.loading-overlay', [
         h('.loading-overlay__container', [
           h(Spinner, {
-            color: '#F7C06C',
+            color: 'rgb(25, 131, 179)',
           }),
 
           this.renderMessage(),
