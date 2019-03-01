58ae24d9229154ba0b463567077c2afd138f0b04
Unknown <mail@mathyn.com>
2019-03-01 15:13:43 +0100

    Changed eth logo to smilo logo

--- ui/app/components/pages/confirm-token-transaction-base/confirm-token-transaction-base.component.js
+++ ui/app/components/pages/confirm-token-transaction-base/confirm-token-transaction-base.component.js
@@ -72,7 +72,7 @@ export default class ConfirmTokenTransactionBase extends Component {
       <div>
         <span>{ `${tokensText} + ` }</span>
         <img
-          src="/images/eth.svg"
+          src="/images/smilo_coin.png"
           height="18"
         />
         <span>{ ethTransactionTotal }</span>
