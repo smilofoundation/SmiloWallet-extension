e7a01b7cb8dd43e7098e0bb29f89e8d4d7f76171
Unknown <mail@mathyn.com>
2019-03-11 09:53:57 +0100

    Fixed explorer link for main net

--- ui/lib/account-link.js
+++ ui/lib/account-link.js
@@ -1,11 +1,13 @@
+import { MAINNET_CODE, TESTNET_CODE } from "../../app/scripts/controllers/network/enums";
+
 module.exports = function (address, network) {
   const net = parseInt(network)
   let link
   switch (net) {
-    case 1: // main net
+    case MAINNET_CODE: // main net
       link = `https://explorer.smilo.network/addr/${address}`
       break
-    case 2: // test net
+    case TESTNET_CODE: // test net
       link = `https://testnet-explorer.smilo.network/addr/${address}`
       break
     default:
--- ui/lib/smilo-explorer-linker.js
+++ ui/lib/smilo-explorer-linker.js
@@ -17,7 +17,7 @@ function getPrefix(networkType) {
     let prefix
     switch (networkType) {
       case "mainnet": // main net
-        prefix = 'testnet-' // TODO: fix at later time when mainnet is live
+        prefix = ''
         break
       case "testnet": // ropsten test net
         prefix = 'testnet-'
