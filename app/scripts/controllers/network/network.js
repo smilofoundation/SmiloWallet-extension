cbc75bba3477c0223f27f2583ee64bf373ba669b
Unknown <mail@mathyn.com>
2019-04-23 16:40:15 +0200

    Fixed crash

--- app/scripts/controllers/network/network.js
+++ app/scripts/controllers/network/network.js
@@ -30,15 +30,6 @@ const SMILO_PROVIDER_TYPES = [MAINNET, TESTNET]
 const env = process.env.METAMASK_ENV
 const METAMASK_DEBUG = process.env.METAMASK_DEBUG
 
-let defaultProviderConfigType
-if (process.env.IN_TEST === 'true') {
-  defaultProviderConfigType = LOCALHOST
-} else if (METAMASK_DEBUG || env === 'test') {
-  defaultProviderConfigType = RINKEBY
-} else {
-  defaultProviderConfigType = MAINNET
-}
-
 const defaultProviderConfig = {
   type: MAINNET,
   ticker: "XSM"
