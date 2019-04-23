139e3358b7ebd9c8e0e095a6a9f5940326a38d48
Unknown <mail@mathyn.com>
2019-04-23 15:10:19 +0200

    Renamed exported seed phrase file

--- ui/app/pages/first-time-flow/seed-phrase/confirm-seed-phrase/confirm-seed-phrase.component.js
+++ ui/app/pages/first-time-flow/seed-phrase/confirm-seed-phrase/confirm-seed-phrase.component.js
@@ -40,7 +40,7 @@ export default class ConfirmSeedPhrase extends PureComponent {
   }
 
   handleExport = () => {
-    exportAsFile('MetaMask Secret Backup Phrase', this.props.seedPhrase, 'text/plain')
+    exportAsFile('Smilo Secret Backup Phrase', this.props.seedPhrase, 'text/plain')
   }
 
   handleSubmit = async () => {
--- ui/app/pages/first-time-flow/seed-phrase/reveal-seed-phrase/reveal-seed-phrase.component.js
+++ ui/app/pages/first-time-flow/seed-phrase/reveal-seed-phrase/reveal-seed-phrase.component.js
@@ -22,7 +22,7 @@ export default class RevealSeedPhrase extends PureComponent {
   }
 
   handleExport = () => {
-    exportAsFile('MetaMask Secret Backup Phrase', this.props.seedPhrase, 'text/plain')
+    exportAsFile('Smilo Secret Backup Phrase', this.props.seedPhrase, 'text/plain')
   }
 
   handleNext = event => {
