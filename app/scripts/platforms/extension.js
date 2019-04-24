97152c11dd7c9c67bc7d1353d4d6f3aa681b0d9e
Unknown <mail@mathyn.com>
2019-04-24 09:46:52 +0200

    Fixed explorer link for the notification

--- app/scripts/platforms/extension.js
+++ app/scripts/platforms/extension.js
@@ -77,7 +77,7 @@ class ExtensionPlatform {
     let prefix
     switch (providerType) {
       case "mainnet":
-        prefix = 'testnet-' // Must be changed when main net is released!
+        prefix = ''
         break
       case "testnet":
         prefix = 'testnet-'
@@ -95,6 +95,8 @@ class ExtensionPlatform {
 
     const title = 'Confirmed transaction'
     const message = `Transaction ${nonce} confirmed! View on block explorer.`
+
+    console.log(providerType);
 
     const explorerUrl = `https://${ this._getExplorerLinkPrefix(providerType) }explorer.smilo.network/tx/${txMeta.hash}`
 
